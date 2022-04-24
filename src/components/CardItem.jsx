241e9a5b4cabedb4aae07c64dfcd35929fcebf29
pranav <pranavpk404@gmail.com>
2022-04-24 11:21:41 +0530

    added lazy loading

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -15,7 +15,7 @@ function CardItem({
   return (
     <div className="dark:bg-gray-700  bg-gray-50 mx-5 flex flex-col pt-6 pb-6 md:flex-row rounded-xl transition duration-150  hover:shadow-xl mt-5">
       <div className=" h-64 w-auto md:w-1/2 flex align-middle sm:justify-center">
-        <img className="mx-0 sm:mx-4" src={imgUrl} />
+        <img className="mx-0 sm:mx-4" src={imgUrl} loading="lazy" />
       </div>
       <div className=" dark:text-white w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
         <h3 className="font-semibold text-lg leading-tight ">{title}</h3>
